14a3910ec54acd09effb389a100549e48f0a5dd7
qmfforsjt <h098s02222@chunchon.gwe.hs.kr>
2025-11-25 09:10:42 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -74,8 +74,12 @@ CONTENT_BY_LABEL: dict[str, dict[str, list[str]]] = {
     #   "images": ["https://.../jjampong1.jpg", "https://.../jjampong2.jpg"],
     #   "videos": ["https://youtu.be/XXXXXXXXXXX"]
     # },
-}
 
+lables[0] : {"test" : [중국식 냉면은 맛있어], "imige" : [https://www.esquirekorea.co.kr/resources_old/online/org_online_image/eq/71c93efd-352d-4fb4-8a98-dd1b51475442.jpg]},
+lables[0] : {"test" : [짜장면은 맛있어], "imige" : [https://m.health.chosun.com/site/data/img_dir/2024/08/02/2024080201848_0.jpg]},
+lables[0] : {"test" : [짬뽕은 맛있어], "imige" : [https://www.chosun.com/resizer/v2/YPSL2DAN5VDJ5C3HNXWHKJGCRU.jpg?auth=d98ca02f38dff609ee5b3bee78894f70a11a2b0e7d232c84581ebb04140eeb76&width=616]},
+lables[0] : {"test" : [탕수은 맛있어], "imige" : [https://recipe1.ezmember.co.kr/cache/recipe/2020/07/05/2e0e7c019f283bcc36d34cdee876d15b1.jpg]},
+    }
 # ======================
 # 유틸
 # ======================
